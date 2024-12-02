a271317d4394b1b847a3c6b25b3a24127c69cfaa
Jesus <jquinteros@unsa.edu.pe>
2024-12-02 18:41:54 -0500

    Actualizando proyecto a insercion por lectura de archivo. Y adicionando implementacion de cuadro que muestra el proximo nodo en el dibujado de la insercion

--- display.c
+++ display.c
@@ -3,8 +3,18 @@
 #include "rbtree.h"
 #include <math.h>
 #include <string.h>
+#include "display.h"
 
-extern RBTree* T;
+RBTree* T = NULL;
+int* keys;
+int size;
+int cont = 0;
+
+void setData(int* a, int n){
+  keys = a;
+  size = n;
+  T = createRBTree();
+}
 
 void drawText(const char* text, float x, float y) {
   glRasterPos2f(x, y);
@@ -13,8 +23,24 @@ void drawText(const char* text, float x, float y) {
   }
 }
 
-void drawRoundedRect(float x, float y, float width, float height, float radius) {
+void drawRoundedRectYellow(float x, float y, float width, float height, float radius) {
   glColor3f(1.0f, 1.0f, 0.3f); // Amarillo suave
+  glBegin(GL_POLYGON);
+
+  glVertex2f(x + radius, y);
+  glVertex2f(x + width - radius, y);
+  glVertex2f(x + width, y + radius);
+  glVertex2f(x + width, y + height - radius);
+  glVertex2f(x + width - radius, y + height);
+  glVertex2f(x + radius, y + height);
+  glVertex2f(x, y + height - radius);
+  glVertex2f(x, y + radius);
+
+  glEnd();
+}
+
+void drawRoundedRectWhite(float x, float y, float width, float height, float radius) {
+  glColor3f(1.0f, 1.0f, 1.0f); // Blanco
   glBegin(GL_POLYGON);
 
   glVertex2f(x + radius, y);
@@ -57,7 +83,7 @@ void drawNode(float x, float y, int value, Color color) {
 }
 
 void drawEdge(float x1, float y1, float x2, float y2) {
-  glColor3f(0.0f, 0.0f, 0.0f); // Arista negra
+  glColor3f(0.0f, 0.0f, 0.0f);
   glBegin(GL_LINES);
   glVertex2f(x1, y1);
   glVertex2f(x2, y2);
@@ -67,47 +93,64 @@ void drawEdge(float x1, float y1, float x2, float y2) {
 void drawTree(RBNode* node, float x, float y, float offsetX, float offsetY) {
   if (node == NULL) return;
 
-  // Dibujar nodo actual
   drawNode(x, y, node->key, node->color);
 
-  // Dibujar subárbol izquierdo
   if (node->left != NULL) {
     float leftX = x - offsetX;
     float leftY = y - offsetY;
-    drawEdge(x, y, leftX, leftY); // Dibujar arista hacia hijo izquierdo
+    drawEdge(x, y, leftX, leftY);
     drawTree(node->left, leftX, leftY, offsetX / 2, offsetY);
   }
 
-  // Dibujar el subárbol derecho
   if (node->right != NULL) {
     float rightX = x + offsetX;
     float rightY = y - offsetY;
-    drawEdge(x, y, rightX, rightY); // Dibujar arista hacia hijo derecho
+    drawEdge(x, y, rightX, rightY);
     drawTree(node->right, rightX, rightY, offsetX / 2, offsetY);
   }
 }
 
 void keyboard(unsigned char key, int x, int y) {
-  if(key == 'i') {
-    int value;
-    printf("Value insert: ");
-    scanf("%d", &value);
-    insertRBNode(T, createRBNode(value));
-    glutPostRedisplay();
-  }
-  if(key == 27) {
+  if(cont >= size)
     exit(0);
+  else if(key == 's') {
+    insertRBNode(T, createRBNode(keys[cont++]));
+    glutPostRedisplay();
+  } else if(key == 'f'){
+    while(cont < size)
+      insertRBNode(T, createRBNode(keys[cont++]));
+    glutPostRedisplay();
+  } else if(key == '\e')
+    exit(0);
+}
+
+void drawNextValue() {
+  if(cont < size) {
+    char buffer[5];
+    snprintf(buffer, sizeof(buffer), "%d", keys[cont]);
+    glColor3f(0.0f, 0.0f, 0.0f);
+    if(keys[cont])
+      if(-10 < keys[cont] && keys[cont] < 10) {
+        drawText(buffer, 0.789f, 0.83f); // Centrar 1 cifra
+      }else {
+        drawText(buffer, 0.778f, 0.83f); // Centrar 2 cifras (3 cifras no centra)
+      }
   }
 }
 
 void display() {
   glClear(GL_COLOR_BUFFER_BIT);
 
-  drawRoundedRect(-0.98f, 0.75f, 0.4f, 0.2f, 0.03f);
+  drawRoundedRectYellow(-0.98f, 0.75f, 0.4f, 0.2f, 0.03f);
+  drawRoundedRectYellow(0.56f, 0.8f, 0.18f, 0.1f, 0.02f);
+  drawRoundedRectWhite(0.75f, 0.8f, 0.1f, 0.1f, 0.02f);
 
   glColor3f(0.0f, 0.0f, 0.0f);
-  drawText("Insert -> (i)", -0.9f, 0.87f);
-  drawText("Exit -> (Esc)", -0.9f, 0.80f);
+  drawText("Full -> (f)", -0.856f, 0.87f);
+  drawText("Step -> (s)", -0.88f, 0.80f);
+  drawText("Next", 0.607f, 0.83f);
+
+  drawNextValue();
 
   if (T && T->root) {
     float startX = 0.0f;   // Coordenada X inicial (root)
@@ -119,4 +162,3 @@ void display() {
 
   glFlush();
 }
-
--- display.h
+++ display.h
@@ -1,7 +1,10 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+#include "rbtree.h"
+
 void display();
 void keyboard(unsigned char key, int x, int y);
+void setData(int* keys, int n);
 
 #endif
